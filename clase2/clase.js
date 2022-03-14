cf0752012fc5108716a7ab2aaf80c3a7619eadf9
NestorDanielGomez <87837253+NestorDanielGomez@users.noreply.github.com>
2022-03-14 19:08:48 -0300

    first commit

---
+++ clase2/clase.js
@@ -0,0 +1,10 @@
+// var i = "son una variable global";
+
+// function foo() {
+//   var i = "son una variable local";
+//   console.log(i);
+// }
+
+// foo();
+// console.log(`*** ${i}`);
+const pepe = new Promise();
