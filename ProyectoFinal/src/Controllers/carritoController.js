de1bf9058c154e79920b67a26ecdd708640ff9cf
NestorDanielGomez <87837253+NestorDanielGomez@users.noreply.github.com>
2022-04-26 23:23:05 -0300

    casi

--- ProyectoFinal/src/Controllers/carritoController.js
+++ ProyectoFinal/src/Controllers/carritoController.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
+const { ProductosController } = require("../Controllers/productosController");
 
-class Carrito {
+class Cart {
   constructor(nombreArchivo) {
     this.archivo = nombreArchivo;
   }
@@ -15,37 +16,28 @@ class Carrito {
     await fs.promises.writeFile(this.archivo, JSON.stringify(data, null, "\t"));
   }
 
-  async save(objeto) {
-    const productos = await this.getData();
-
-    let id;
-    let timestamp;
-
-    const productonuevo = {
+  async save() {
+    const allCarts = await this.getData();
+    const nuevocarro = {
       id: uuidv4(),
       timestamp: Date.now(),
-      nombre: objeto.nombre,
-      descripcion: objeto.descripcion,
-      codigo: objeto.codigo,
-      foto: objeto.foto,
-      precio: objeto.precio,
-      stock: objeto.stock,
+      usercart: [],
     };
-    productos.push(productonuevo);
-    await this.saveData(productos);
+    allCarts.push(nuevocarro);
+    await this.saveData(allCarts);
+    return nuevocarro.id;
   }
 
-  async getById(idProducto) {
-    const productos = await this.getData();
-
-    const indice = productos.findIndex((producto) => {
-      if (producto.id === idProducto) return true;
+  async getById(idCart) {
+    const carritos = await this.getData();
+    const indice = carritos.findIndex((carrito) => {
+      if (carrito.id === idCart) return true;
       else return false;
     });
 
     if (indice === -1) return null;
 
-    return productos[indice];
+    return carritos[indice];
   }
 
   async getAll() {
@@ -53,44 +45,27 @@ class Carrito {
     return productos;
   }
 
-  async deleteById(number) {
-    const productos = await this.getData();
+  async deleteById(cart_to_delete) {
+    const ALLCARTS = await this.getData();
 
-    const nuevoArray = productos.filter(
-      (unProducto) => unProducto.id != number
+    const NEW_ALLCARTS = ALLCARTS.filter((cart) => cart.id != cart_to_delete);
+
+    await this.saveData(NEW_ALLCARTS);
+  }
+
+  async deleteProductOfCart(idcart, idproduct) {
+    const FULLCART = await this.getById(idcart);
+    const productosdecarrito = FULLCART.usercart;
+    const nuevoArray = productosdecarrito.filter(
+      (unProducto) => unProducto.id != idproduct
     );
 
-    await this.saveData(nuevoArray);
-  }
-
-  async deleteAll() {
-    const listadolimpio = [];
-
-    await this.save(listadolimpio);
-  }
-
-  async Update(id, nuevaData) {
-    const productos = await this.getData();
-
-    const indice = productos.findIndex((unProducto) => unProducto.id === id);
-
-    if (indice < 0) throw new Error("no existe el producto");
-
-    const productoActualizado = {
-      id,
-      ...nuevaData,
-    };
-
-    productos.splice(indice, 1, productoActualizado);
-
-    await this.saveData(productos);
-
-    return productoActualizado;
+    console.log(nuevoArray);
   }
 }
 
-const miCarrito = new Carrito(`src/basedatos.json`);
+const miCarrito = new Cart(`src/carrito.json`);
 
 module.exports = {
-  MiCarrito: miCarrito,
+  CartController: miCarrito,
 };
--- ProyectoFinal/src/routes/carrito.js
+++ ProyectoFinal/src/routes/carrito.js
@@ -1,42 +1,82 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
-
-let carrito = [];
+const { CartController } = require("../Controllers/carritoController");
+const { ProductosController } = require("../Controllers/productosController");
+let userCart = [];
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const ALLCARTS = await CartController.getAll();
   res.json({
     msj: "mi carrito",
+    data: ALLCARTS,
   });
 });
 
 router.get("/:id", async (req, res) => {
-  const producto = await ProductosController.getById(req.params.id);
-  if (!producto)
+  const CART = await CartController.getById(req.params.id);
+
+  if (!CART)
     return res.status(404).json({
-      msj: "producto no encontrado",
+      msj: "El Carrito no existe",
+    });
+
+  if (CART.usercart.length == 0)
+    return res.status(404).json({
+      msj: "El Carrito esta vacio",
     });
 
   res.json({
-    data: producto,
+    data: CART,
   });
 });
 
-router.post("/", (req, res) => {
-  const body = req.body;
+router.get("/:id/productos", async (req, res) => {
+  const CART = await CartController.getById(req.params.id);
 
-  const micarrito = {
-    id: uuidv4(),
-    timestamp: Date.now(),
-    nombre: body.nombre,
-    raza: body.raza,
-    edad: body.edad,
-  };
+  // if (!CART.usercart)
+  //   return res.status(404).json({
+  //     msj: "carrito vacio",
+  //   });
 
-  mascotas.push(micarrito);
   res.json({
-    nuevaMascota,
+    data: CART.usercart,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const AddCart = await CartController.save();
+
+  res.json({
+    msg: "ID del nuevo carrito",
+    id: AddCart,
+  });
+});
+
+// router.post("/:id/productos/:id_prod", async (req, res) => {
+//   const AddCart = await CartController.save();
+
+//   res.json({
+//     msg: "ID del nuevo carrito",
+//     id: AddCart,
+//   });
+// });
+
+router.delete("/:id", async (req, res) => {
+  const resultado = await CartController.deleteById(req.params.id);
+  res.json({
+    msg: "producto eliminado",
+  });
+});
+
+router.delete("/:id/productos/:id_prod", async (req, res) => {
+  const array = await CartController.deleteProductOfCart(
+    req.params.id,
+    req.params.id_prod
+  );
+
+  res.json({
+    data: array,
   });
 });
 
