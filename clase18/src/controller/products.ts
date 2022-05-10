0b433f8d84dc8622f098375bfd5d304142937212
NestorDanielGomez <87837253+NestorDanielGomez@users.noreply.github.com>
2022-05-09 22:39:21 -0300

    clase18

---
+++ clase18/src/controller/products.ts
@@ -0,0 +1,21 @@
+import { ProductsModel } from "../services/database";
+
+export const getAllProducts = async () => {
+  const products = await ProductsModel.get();
+
+  return products;
+};
+
+export const createProduct = async (
+  name: string,
+  stock: number,
+  price: number
+) => {
+  const newProduct = await ProductsModel.create({
+    name,
+    stock,
+    price,
+  });
+
+  return newProduct;
+};
